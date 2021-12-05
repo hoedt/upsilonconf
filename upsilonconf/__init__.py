753084a43241af8025aa4eda0b071bd80c1834d1
Pieter-Jan Hoedt <hoedt@ml.jku.at>
2021-12-05 21:55:45 +0100

    include file operations in package

--- upsilonconf/__init__.py
+++ upsilonconf/__init__.py
@@ -1,6 +1,7 @@
 """ A simple configuration library. """
 
 from .config import Configuration
-from .errors import InvalidKeyError
+from .errors import *
+from .persistence import load, save
 
-__version__ = "0.1.0"
+__version__ = "0.2.0.dev1"
