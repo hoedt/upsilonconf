d92053f275217d94869a62eba6ff57df316cd889
Pieter-Jan Hoedt <hoedt@ml.jku.at>
2021-12-05 19:18:06 +0100

    add decorator for optional imports

---
+++ tests/test__utils.py
@@ -0,0 +1,83 @@
+from unittest import TestCase
+
+from upsilonconf.errors import OptionalDependencyError
+from upsilonconf._utils import optional_dependency_to
+
+
+IMPORT_KEY = "import"
+
+
+def import_valid(*args, **kwargs):
+    import sys
+
+    kwargs[IMPORT_KEY] = sys
+    return args, kwargs
+
+
+def import_invalid(*args, **kwargs):
+    import sys.invalid
+
+    kwargs[IMPORT_KEY] = sys.invalid
+    return args, kwargs
+
+
+class Test(TestCase):
+    def test_optional_dependency_to(self):
+        wrapper = optional_dependency_to()
+        _import_valid = wrapper(import_valid)
+        args, kwargs = _import_valid()
+        self.assertTupleEqual((), args)
+        self.assertIn(IMPORT_KEY, kwargs)
+
+    def test_optional_dependency_to_import_error(self):
+        wrapper = optional_dependency_to()
+        _import_invalid = wrapper(import_invalid)
+        with self.assertRaisesRegex(OptionalDependencyError, "install '.*' to"):
+            _import_invalid()
+
+    def test_optional_dependency_to_with_arguments(self):
+        wrapper = optional_dependency_to()
+        _import_valid = wrapper(import_valid)
+        args, kwargs = (1, "test", object()), {"kwarg1": 1, "kwarg2": 0.2}
+        _args, _kwargs = _import_valid(*args, **kwargs)
+        self.assertTupleEqual(args, _args)
+        self.assertIn(IMPORT_KEY, _kwargs)
+        _kwargs.pop(IMPORT_KEY)
+        self.assertEqual(kwargs, _kwargs)
+
+    def test_optional_dependency_to_import_error_with_arguments(self):
+        wrapper = optional_dependency_to()
+        _import_invalid = wrapper(import_invalid)
+        args, kwargs = (1, "test", object()), {"kwarg1": 1, "kwarg2": 0.2}
+        with self.assertRaisesRegex(OptionalDependencyError, "install '.*' to"):
+            _import_invalid(args, kwargs)
+
+    def test_optional_dependency_to_message(self):
+        msg = "test imports"
+        wrapper = optional_dependency_to(msg)
+        _import_valid = wrapper(import_valid)
+        args, kwargs = _import_valid()
+        self.assertTupleEqual((), args)
+        self.assertIn(IMPORT_KEY, kwargs)
+
+    def test_optional_dependency_to_import_error_message(self):
+        msg = "test imports"
+        wrapper = optional_dependency_to(msg)
+        _import_invalid = wrapper(import_invalid)
+        with self.assertRaisesRegex(OptionalDependencyError, msg):
+            _import_invalid()
+
+    def test_optional_dependency_to_package(self):
+        pkg = "sys-invalid"
+        wrapper = optional_dependency_to(package=pkg)
+        _import_valid = wrapper(import_valid)
+        args, kwargs = _import_valid()
+        self.assertTupleEqual((), args)
+        self.assertIn(IMPORT_KEY, kwargs)
+
+    def test_optional_dependency_to_import_error_package(self):
+        pkg = "sys-invalid"
+        wrapper = optional_dependency_to(package=pkg)
+        _import_invalid = wrapper(import_invalid)
+        with self.assertRaisesRegex(OptionalDependencyError, f"install '{pkg}' to"):
+            _import_invalid()
---
+++ upsilonconf/_utils.py
@@ -0,0 +1,56 @@
+from upsilonconf.errors import OptionalDependencyError
+
+
+def optional_dependency_to(feature: str = None, package: str = None) -> callable:
+    """
+    Decorator factory for functions that provide optional features.
+
+    The decorators produced by this decorator factory aim
+    to capture any import errors that might occur
+    as a result of optional dependencies not being installed.
+    If the import statement fails, an `OptionalDependencyError` is raised
+    with a (hopefully) more meaningful error message.
+
+    Parameters
+    ----------
+    feature : str, optional
+        A description of the optional feature that could fail.
+        This description should complete the sentence
+        "You have to install a package to ...".
+        If no description is provided, a generic description is used.
+    package : str, optional
+        The name of the package that needs to be installed
+        to make use of the optional features.
+        By default, the module name is used to identify the package.
+        However, sometimes there might be a (slight) difference.
+
+    Returns
+    -------
+    _decorator : callable
+        A decorator that wraps the
+
+    Raises
+    ------
+    OptionalDependencyError
+        If the import of an optional dependency in the function failed.
+    """
+    if feature is None:
+        feature = "make use of this feature"
+
+    def _decorator(func: callable):
+        def _wrapper(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except ImportError as err:
+                pkg = err.name if package is None else package
+                msg = f"You have to install '{pkg}' to {feature}"
+                raise OptionalDependencyError(
+                    msg, name=err.name, path=err.path
+                ) from err
+
+        return _wrapper
+
+    return _decorator
+
+
+optional_dependency = optional_dependency_to(None)
