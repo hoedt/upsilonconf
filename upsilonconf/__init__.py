ee4257c87815d0f1bb6742251d72d4b3c66cff8a
Pieter-Jan Hoedt <hoedt@ml.jku.at>
2022-09-17 10:13:57 +0200

    introduce aliases for wildcard import

--- upsilonconf/__init__.py
+++ upsilonconf/__init__.py
@@ -4,4 +4,15 @@ from ._optional_dependency import OptionalDependencyError
 from .io import load, save, from_cli
 from .config import *
 
-__version__ = "0.4.1"
+__version__ = "0.4.2.dev1"
+__all__ = config.__all__ + [
+    "OptionalDependencyError",
+    "load_config",
+    "save_config",
+    "config_from_cli",
+]
+
+# aliases
+load_config = load
+save_config = save
+config_from_cli = from_cli
