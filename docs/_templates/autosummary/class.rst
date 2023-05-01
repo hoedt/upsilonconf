a5a22b1e0458a5ffa5d048142cedf98d89435355
Pieter-Jan Hoedt <hoedt@ml.jku.at>
2023-05-01 19:53:56 +0200

    finetune style of docs

---
+++ docs/_templates/autosummary/class.rst
@@ -0,0 +1,12 @@
+.. currentmodule:: {{module}}
+
+.. testsetup::
+
+    from upsilonconf import *
+
+
+{{ name | underline }}
+
+.. autoclass:: {{name}}
+    :members:
+
--- docs/conf.py
+++ docs/conf.py
@@ -15,16 +15,17 @@ from datetime import date
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
 
-project = 'UpsilonConf'
-copyright = f'2022-{date.today().year}, Pieter-Jan Hoedt'
-author = 'Pieter-Jan Hoedt'
+project = "UpsilonConf"
+copyright = f"2022-{date.today().year}, Pieter-Jan Hoedt"
+author = "Pieter-Jan Hoedt"
 
 # The full version, including alpha/beta/rc tags
 import upsilonconf
+
 release = upsilonconf.__version__
 
 
@@ -39,13 +40,17 @@ extensions = [
     "numpydoc",
 ]
 
+autodoc_member_order = "bysource"
+numpydoc_show_class_members = False
+numpydoc_class_members_toctree = False
+
 # Add any paths that contain templates here, relative to this directory.
-templates_path = []  # ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -53,7 +58,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
